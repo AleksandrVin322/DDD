b70d66e692fc77e032a4dce86389668832de17ca
AleksandrVin322 <Alexandr.Vinocurov@yandex.ru>
2025-01-10 22:03:55 +0300

    хуярим дальше

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,6 @@ import 'package:ddd/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainScreen());
+  runApp(
+      const MaterialApp(debugShowCheckedModeBanner: false, home: MainScreen()));
 }
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -7,14 +7,14 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _MainScreenState();
 }
 
-var featch1 =
-    Featch(anal: 'Info Anal_1', dev: 'Info Dev_1', test: 'Info Test_1');
-var featch2 =
-    Featch(anal: 'Info Anal_2', dev: 'Info Dev_2', test: 'Info Test_2');
-var featch3 =
-    Featch(anal: 'Info Anal_3', dev: 'Info Dev_3', test: 'Info Test_3');
-var featch4 =
-    Featch(anal: 'Info Anal_4', dev: 'Info Dev_4', test: 'Info Test_4');
+var featch1 = Featch(
+    anal: 'Info Anal_1', dev: 'Info Dev_1', test: 'Info Test_1', name: '1');
+var featch2 = Featch(
+    anal: 'Info Anal_2', dev: 'Info Dev_2', test: 'Info Test_2', name: '2');
+var featch3 = Featch(
+    anal: 'Info Anal_3', dev: 'Info Dev_3', test: 'Info Test_3', name: '3');
+var featch4 = Featch(
+    anal: 'Info Anal_4', dev: 'Info Dev_4', test: 'Info Test_4', name: '4');
 List<Featch> listFeatch = [featch1, featch2, featch3, featch4];
 
 class _MainScreenState extends State<MainScreen> {
@@ -26,7 +26,7 @@ class _MainScreenState extends State<MainScreen> {
 
   void changeFeatch(int index) {
     i = index;
-    nameFeatch = 'Featch #${index + 1}';
+    nameFeatch = 'Featch #${listFeatch[index].name}';
     setState(() {});
   }
 
@@ -69,116 +69,217 @@ class _MainScreenState extends State<MainScreen> {
     setState(() {});
   }
 
+  final controllerAnal = TextEditingController();
+  final controllerDev = TextEditingController();
+  final controllerTest = TextEditingController();
+  final controllerName = TextEditingController();
+
+  void saveFeatch() {
+    var textAnal = controllerAnal.text;
+    var textDev = controllerDev.text;
+    var textTest = controllerTest.text;
+    var textName = controllerName.text;
+    var featch =
+        Featch(anal: textAnal, dev: textDev, test: textTest, name: textName);
+    listFeatch.add(featch);
+    setState(() {});
+    controllerAnal.text = '';
+    controllerDev.text = '';
+    controllerTest.text = '';
+    controllerName.text = '';
+    Navigator.of(context).pop();
+  }
+
+  alert(context) => showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Center(
+            child: Text('Create new featch'),
+          ),
+          content: Column(
+            children: [
+              SizedBox(height: 20),
+              Text('Name featch'),
+              TextField(controller: controllerName),
+              SizedBox(height: 20),
+              Text('Anal'),
+              Row(
+                children: [
+                  Expanded(child: TextField(controller: controllerAnal)),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.add))
+                ],
+              ),
+              SizedBox(height: 20),
+              Text('Dev'),
+              Row(
+                children: [
+                  Expanded(child: TextField(controller: controllerDev)),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.add))
+                ],
+              ),
+              SizedBox(height: 20),
+              Text('Test'),
+              Row(
+                children: [
+                  Expanded(child: TextField(controller: controllerTest)),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.add))
+                ],
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(onPressed: saveFeatch, child: Text('Save')),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('Cancel'))
+          ],
+        ),
+      );
+
+  void replaceFlexAll() {
+    flexAnal = flexDev = flexTest = 1;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Row(
-          children: [
-            /// Колонка слева с фичами
-            Expanded(
-              child: Column(
-                children: [
-                  Text(
-                    nameFeatch,
-                    style: TextStyle(fontSize: 20),
+    return Scaffold(
+      floatingActionButton:
+          IconButton(onPressed: replaceFlexAll, icon: Icon(Icons.replay)),
+      backgroundColor: Colors.white,
+      body: Row(
+        children: [
+          /// Колонка слева с фичами
+          Expanded(
+            child: Column(
+              children: [
+                Text(
+                  nameFeatch,
+                  style: TextStyle(fontSize: 20),
+                ),
+                SizedBox(height: 20),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: listFeatch.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return TextButton(
+                          onPressed: () {
+                            changeFeatch(index);
+                          },
+                          child: Text('Featch #${listFeatch[index].name}'));
+                    },
                   ),
-                  SizedBox(height: 20),
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: listFeatch.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return TextButton(
-                            onPressed: () {
-                              changeFeatch(index);
-                            },
-                            child: Text('Featch #${index + 1}'));
-                      },
-                    ),
-                  ),
-                  IconButton(
+                ),
+                Row(
+                  children: [
+                    IconButton(
                       onPressed: () {
                         deleteCurrentFeatch(i);
                       },
-                      icon: Icon(Icons.delete)),
-                ],
-              ),
+                      icon: Icon(Icons.delete),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        alert(context);
+                      },
+                      icon: Icon(Icons.plus_one),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.recycling),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            Container(
-              color: Colors.black,
-              width: 1,
-              height: double.infinity,
-            ),
+          ),
+          Container(
+            color: Colors.black,
+            width: 1,
+            height: double.infinity,
+          ),
 
-            /// Колонка справа с таблицей
-            Expanded(
-              flex: 15,
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: flexAnal,
-                    child: Column(
-                      children: [
-                        TextButton(
-                            child: Text(
-                              'Anal',
-                              style: TextStyle(
-                                  overflow: TextOverflow.ellipsis,
-                                  fontSize: 20),
-                            ),
-                            onPressed: changeFlexAnal),
-                        Text('${listFeatch[i].anal}'),
-                      ],
-                    ),
+          /// Колонка справа с таблицей
+          Expanded(
+            flex: 14,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: flexAnal,
+                  child: Column(
+                    children: [
+                      flexAnal == 1 && flexDev == 20 && flexTest == 20
+                          ? IconButton(
+                              onPressed: changeFlexAnal,
+                              icon: Icon(Icons.analytics))
+                          : TextButton(
+                              child: Text(
+                                'Anal',
+                                style: TextStyle(
+                                    overflow: TextOverflow.ellipsis,
+                                    fontSize: 20),
+                              ),
+                              onPressed: changeFlexAnal),
+                      Text('${listFeatch[i].anal}'),
+                    ],
                   ),
-                  Container(
-                    color: Colors.black,
-                    width: 1,
-                    height: double.infinity,
+                ),
+                Container(
+                  color: Colors.black,
+                  width: 1,
+                  height: double.infinity,
+                ),
+                Expanded(
+                  flex: flexDev,
+                  child: Column(
+                    children: [
+                      flexAnal == 20 && flexDev == 1 && flexTest == 20
+                          ? IconButton(
+                              onPressed: changeFlexDev,
+                              icon: Icon(Icons.developer_board))
+                          : TextButton(
+                              child: Text(
+                                'Dev',
+                                style: TextStyle(
+                                    overflow: TextOverflow.ellipsis,
+                                    fontSize: 20),
+                              ),
+                              onPressed: changeFlexDev),
+                      Text('${listFeatch[i].anal}'),
+                    ],
                   ),
-                  Expanded(
-                    flex: flexDev,
-                    child: Column(
-                      children: [
-                        TextButton(
-                            child: Text(
-                              'Dev',
-                              style: TextStyle(
-                                  overflow: TextOverflow.ellipsis,
-                                  fontSize: 20),
-                            ),
-                            onPressed: changeFlexDev),
-                        Text('${listFeatch[i].anal}'),
-                      ],
-                    ),
+                ),
+                Container(
+                  color: Colors.black,
+                  width: 1,
+                  height: double.infinity,
+                ),
+                Expanded(
+                  flex: flexTest,
+                  child: Column(
+                    children: [
+                      flexAnal == 20 && flexDev == 20 && flexTest == 1
+                          ? IconButton(
+                              onPressed: changeFlexDev,
+                              icon: Icon(Icons.article))
+                          : TextButton(
+                              child: Text(
+                                'Test',
+                                style: TextStyle(
+                                    overflow: TextOverflow.ellipsis,
+                                    fontSize: 20),
+                              ),
+                              onPressed: changeFlexTest),
+                      Text('${listFeatch[i].anal}'),
+                    ],
                   ),
-                  Container(
-                    color: Colors.black,
-                    width: 1,
-                    height: double.infinity,
-                  ),
-                  Expanded(
-                    flex: flexTest,
-                    child: Column(
-                      children: [
-                        TextButton(
-                            child: Text(
-                              'Test',
-                              style: TextStyle(
-                                  overflow: TextOverflow.ellipsis,
-                                  fontSize: 20),
-                            ),
-                            onPressed: changeFlexTest),
-                        Text('${listFeatch[i].anal}'),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -188,10 +289,12 @@ class Featch {
   late String anal;
   late String dev;
   late String test;
+  late String name;
 
   Featch({
     required this.anal,
     required this.dev,
     required this.test,
+    required this.name,
   });
 }
